62cc348f03b69559fdf0cb4e41db079a90001f48
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-12 20:51:24 -0500

    commit tree 9943d6d02875df529f7d64761f7e65361f0c6b7c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+    count = integer
+    while count >= 1
+        puts "#{count} SECOND(S)!"
+        # sleep(5.seconds)
+        count -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep integer
+    count = integer
+    while count > 0
+        puts "#{count} SECOND(S)!"
+        sleep 1
+        count -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
